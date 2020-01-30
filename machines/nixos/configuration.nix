06c13c307e42c945c45c1ea09ff315b630efa4fa
Quentin Gliech <quentingliech@gmail.com>
2020-01-30 02:01:44 +0100

    nixos: add ssh key

--- machines/nixos/configuration.nix
+++ machines/nixos/configuration.nix
@@ -121,6 +121,10 @@
       home = "/home/sandhose";
       shell = pkgs.zsh;
       extraGroups = [ "wheel" ];
+
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKI3JrkOofavtPW8jV/GYM5Mv1gn/h732JPm82SGGj50 sandhose@sandhose-laptop"
+      ];
     };
   };
 
