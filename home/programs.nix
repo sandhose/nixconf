8d543365638e44a0bb4bd2d5922afd3f00ff4f85
Quentin Gliech <quentingliech@gmail.com>
2020-02-18 11:50:29 +0100

    git: pull rebase & autostash

--- home/programs.nix
+++ home/programs.nix
@@ -12,6 +12,11 @@ with builtins; {
       signByDefault = true;
     };
 
+    extraConfig = {
+      pull.rebase = true;
+      rebase.autoStash = true;
+    };
+
     userEmail = "quentingliech@gmail.com";
     userName = "Quentin Gliech";
   };
