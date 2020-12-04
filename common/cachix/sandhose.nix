2aca807e3c2fb6d571c27d6279e7ca88132ac2c0
Quentin Gliech <quentingliech@gmail.com>
2020-12-04 13:00:49 +0000

    use cachix

---
+++ common/cachix/sandhose.nix
@@ -0,0 +1,12 @@
+
+{
+  nix = {
+    binaryCaches = [
+      "https://sandhose.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "sandhose.cachix.org-1:/GXCS3aRHcivifkZY1ntt16h2RSkrx0sjshp0Pj+xjM="
+    ];
+  };
+}
+    
--- common/default.nix
+++ common/default.nix
@@ -2,6 +2,7 @@
 
 {
   imports = [
+    ./cachix.nix
     ../home
   ];
 
