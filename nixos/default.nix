b2e0eb6af498f6b4f64c0eac7557d24da3959140
Quentin Gliech <quentingliech@gmail.com>
2020-12-04 15:05:09 +0000

    nixos: prepare kvm

--- nixos/default.nix
+++ nixos/default.nix
@@ -10,6 +10,12 @@
     };
 
     kernelPackages = pkgs.linuxPackages_latest;
+
+    extraModprobeConfig = ''
+      options kvm_intel nested=1
+      options kvm_intel emulate_invalid_guest_state=0
+      options kvm ignore_msrs=1 report_ignored_msrs=0
+    '';
   };
 
   environment = {
@@ -41,6 +47,7 @@
   };
 
   virtualisation.libvirtd.enable = true;
+  virtualisation.kvmgt.enable = true;
 
   sound.enable = true;
   hardware = {
