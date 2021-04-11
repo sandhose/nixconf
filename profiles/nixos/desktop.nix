968d95e9ac69a272f69c9a0808d1e5df8017e729
Quentin Gliech <quentingliech@gmail.com>
2021-04-11 21:46:26 +0200

    postgres: allow TCP connections

--- profiles/nixos/desktop.nix
+++ profiles/nixos/desktop.nix
@@ -16,6 +16,11 @@
     postgresql = {
       enable = true;
       package = pkgs.postgresql_13;
+      enableTCPIP = true;
+      authentication = pkgs.lib.mkOverride 10 ''
+        local all all trust
+        host all all ::1/128 trust
+      '';
       ensureUsers = [{
         name = "sandhose";
         ensurePermissions = {
