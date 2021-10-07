5a4cac39584f492c1ca764b5301fcce93caf6148
Quentin Gliech <quentingliech@gmail.com>
2021-10-07 18:28:48 +0200

    Force Vulkan to use RADV ICDs
    AMDVLK ones are broken for now? At least in Steam.

--- profiles/nixos/amd.nix
+++ profiles/nixos/amd.nix
@@ -20,6 +20,7 @@
   ];
 
   # Force RADV
-  environment.variables.VK_ICD_FILENAMES =
-    "/run/opengl-driver/share/vulkan/icd.d/radeon_icd.x86_64.json";
+  #environment.variables.VK_ICD_FILENAMES =
+  #  "/run/opengl-driver/share/vulkan/icd.d/radeon_icd.x86_64.json";
+  environment.variables.AMD_VULKAN_ICD = "RADV";
 }
