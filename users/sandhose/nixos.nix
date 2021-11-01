b5efc3c632e820f5e3e56087f88295c7469b92fd
Quentin Gliech <quentingliech@gmail.com>
2021-11-01 21:15:03 +0100

    Add SSH key

--- users/sandhose/nixos.nix
+++ users/sandhose/nixos.nix
@@ -14,6 +14,7 @@
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKI3JrkOofavtPW8jV/GYM5Mv1gn/h732JPm82SGGj50 sandhose@sandhose-laptop"
         "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBACiw9P5PHkzD7MuyUFr51A4OGf8IipB/vEmlgdE2zJeYKD+9zaaEEyZ8LxYFn7RMUEtD0043DJKzjGKR+8n7PucNgF3bGRkuDsaq3T7P2+zaj6jbnGYwm0q+4ltNzuO0gFWUhN399KXn2zZmHqh2SIdnkKOMQwdEl7lD0EAUcM76AQl3w== sandhose-desktop"
+        "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBP84fwAsjyX4ziJvQ4HZesaVRbvhQvCEYPfKBNBSCWHsnnDQn4o85HAdKceVwcNN/wlfm+AuH9pNpoJgpEEfpy4= sandhose@Quentins-MacBook-Pro.local"
       ];
     };
   };
