2b99c37fe10f2fc59b9328a7461c85dbb9488a1b
Quentin Gliech <quentingliech@gmail.com>
2024-03-28 14:18:44 +0100

    Better Rust vim setup

--- profiles/home-manager/vim/config/lua/my/init.lua
+++ profiles/home-manager/vim/config/lua/my/init.lua
@@ -30,8 +30,22 @@ require'nvim-tree'.setup {
   },
 }
 
+local dap, dapui = require'dap', require'dapui'
+dapui.setup()
+dap.listeners.before.attach.dapui_config = function()
+  dapui.open()
+end
+dap.listeners.before.launch.dapui_config = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated.dapui_config = function()
+  dapui.close()
+end
+dap.listeners.before.event_exited.dapui_config = function()
+  dapui.close()
+end
+
 require'my.completion'
-require'my.dap'
 require'my.lsp'
 require'my.lualine'
 require'my.telescope'
