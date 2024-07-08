edf4082b075057fc1943e52a87aa082462f8067f
Quentin Gliech <quentingliech@gmail.com>
2024-07-08 10:43:39 +0200

    Remove x86 overrides on darwin & disable tests for nomad

--- profiles/base/default.nix
+++ profiles/base/default.nix
@@ -9,21 +9,16 @@ in
   nixpkgs.overlays = [
     self.overlay
     fenix.overlays.default
+    # Nomad tests are failing on darwin in CI and often failing because of disk space requirements on Linux
+    (final: prev: {
+      nomad = prev.nomad.overrideAttrs (old: {
+        doChek = false;
+        doInstallCheck = false;
+      });
+    })
     (final: prev: {
       inherit ((import rycee { pkgs = prev; })) firefox-addons;
     })
-    (final: prev:
-      let
-        inherit (prev) config;
-        x86Pkgs = import nixpkgs {
-          inherit config;
-          localSystem = "x86_64-darwin";
-        };
-      in
-      if prev.stdenv.system == "aarch64-darwin" then {
-        inherit (x86Pkgs) xquartz nix-index wrk;
-      } else
-        { })
   ];
 
   nix.registry.nixpkgs.flake = nixpkgs;
